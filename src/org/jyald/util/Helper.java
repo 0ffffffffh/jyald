93c15476aaf9567c0230a625b5a55d85e456195b
Oguz Kartal <0xffffffff@oguzkartal.net>
2011-08-14 17:14:25 +0300

    aahhh. much better.

--- src/org/jyald/util/Helper.java
+++ src/org/jyald/util/Helper.java
@@ -6,19 +6,30 @@ import org.jyald.debuglog.Log;
 
 
 public class Helper {
+	
+	private static String workingDir = null;
+	
 	public static String getWorkingDir() {
-		String path = Helper.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		path = path.substring(1, path.length()-1);
-		path = path.replace("%20", " ");
 		
-		if (path.contains(".")) {
-			int n = path.lastIndexOf("/");
-			path = path.substring(0,n);
+		if (workingDir == null) {
+			String path = Helper.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+			path = path.substring(1, path.length()-1);
+			path = path.replace("%20", " ");
+		
+			if (path.contains(".")) {
+				int n = path.lastIndexOf("/");
+				
+				if (n != -1) {
+					path = path.substring(0,n);
+				}
+			}
+		
+			workingDir = path;
+			
+			Log.write("Executing path detected: %s", workingDir);
 		}
 		
-		Log.write("Executing path detected: %s", path);
-		
-		return path;
+		return workingDir;
 	}
 	
 	public static boolean fileExist(String fileName) {
